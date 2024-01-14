21b63ed0245d96872b464aaffc187765c9e73873
nickolaykondratyev <kondratyev.nickolay@gmail.com>
2024-01-14 15:43:23 -0800

    Convert person to data class

--- src/main/java/pl/piomin/services/model/Person.kt
+++ src/main/java/pl/piomin/services/model/Person.kt
@@ -5,22 +5,14 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.PositiveOrZero
 
-class Person {
+data class Person(
     @kotlin.jvm.JvmField
-    var id: @Max(10000) Int? = null
-    var firstName: @NotBlank String? = null
-    var lastName: @NotBlank String? = null
-    var age: @PositiveOrZero Int = 0
+    var id: @Max(10000) Int? = null,
+    var firstName: @NotBlank String? = null,
+    var lastName: @NotBlank String? = null,
+    var age: @PositiveOrZero Int = 0,
     var gender: @NotNull Gender? = null
-
-    constructor()
-    constructor(id: Int?, firstName: String?, lastName: String?, age: Int, gender: Gender?) {
-        this.id = id
-        this.firstName = firstName
-        this.lastName = lastName
-        this.age = age
-        this.gender = gender
-    }
+) {
 
     override fun toString(): String {
         return "Person{" +
